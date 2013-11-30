11b0f3c47a28b21e8d4291f11ef234bdf5341bc0
Justin Svegliato <justin.svegliato1@marist.edu>
2013-11-29 23:19:43 -0500

    Cleaned up code

--- public_html/assets/js/system/os/display/hard-drive-display.js
+++ public_html/assets/js/system/os/display/hard-drive-display.js
@@ -1,12 +1,12 @@
 function HardDriveDisplay() {};
 
 HardDriveDisplay.hardDrive = $("#hard-drive-display");
-HardDriveDisplay.hardDriveTable = $("#hard-drive-display table tbody");
+HardDriveDisplay.hardDriveTable = $("#hard-drive-display tbody");
 
 HardDriveDisplay.row = "<tr> \
-                            <td class='location-value'>{0}</td> \
-                            <td class='header-value'>{1}</td> \
-                            <td class='data-value'>{2}</td> \
+                            <td class='text-muted location-value'><small><b>{0}</b></small></td> \
+                            <td class='text-warning header-value'><small>{1}</small></td> \
+                            <td class='text-primary data-value'><small>{2}</small></td> \
                         </tr>";
 
 HardDriveDisplay.update = function() {    
@@ -15,7 +15,7 @@ HardDriveDisplay.update = function() {
     for (var i = 0; i < HardDriveManager.TRACKS; i++) {
         for (var j = 0; j < HardDriveManager.SECTORS; j++) {
             for (var k = 0; k < HardDriveManager.BLOCKS; k++) {
-                var data = HardDriveManager.read(i, j, k);
+                var data = HardDriveManager.hardDrive.read(i, j, k);
                 var key = HardDrive.getKey(i, j, k);
                 var row = HardDriveDisplay.row.format(
                         HardDrive.getKey(i, j, k),
