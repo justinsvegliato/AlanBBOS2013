d4a900511f1f863926464ee28c5ebc50591844d0
Justin Svegliato <justin.svegliato1@marist.edu>
2013-11-29 23:15:13 -0500

    Added functionality to load processes to the hard drive

--- public_html/assets/js/system/os/process-manager.js
+++ public_html/assets/js/system/os/process-manager.js
@@ -12,11 +12,11 @@ ProcessManager.load = function(program) {
     // If there is space available to be allocated, do so. Otherwise send an interrupt to the kernel
     if (Object.keys(ProcessManager.processControlBlocks).length < MemoryManager.NUMBER_OF_BLOCKS) {
         // Create a new pcb and add it to the ready queue
-        var pcb = new ProcessControlBlock();
+        var pcb = new ProcessControlBlock(true);
         ProcessManager.processControlBlocks[pcb.processId] = pcb;
-        
+
         // Set the base and limit of the pcb
-        MemoryManager.allocate(pcb);               
+        MemoryManager.allocate(pcb);
 
         // Load the program into memory
         var components = program.split(/\s+/);
@@ -26,13 +26,25 @@ ProcessManager.load = function(program) {
 
         return pcb;
     } else {
-        Kernel.handleInterupts(PROCESS_LOAD_FAULT_IRQ, "Insufficient memory");
+        var pcb = new ProcessControlBlock(false);
+        ProcessManager.processControlBlocks[pcb.processId] = pcb;
+        
+        var modifiedProgram = program.split(/\s+/).join('');
+        Kernel.handleInterupts(DISK_OPERATION_IRQ, ["loadProcess", pcb, modifiedProgram]);
+        return pcb;
     }
+//    else {
+//        Kernel.handleInterupts(PROCESS_LOAD_FAULT_IRQ, "Insufficient memory");
+//    }
 };
 
 // Unloads the specified program from memory
 ProcessManager.unload = function(pcb) {
-    MemoryManager.deallocate(pcb);
+    if (pcb.inMemory) {
+        MemoryManager.deallocate(pcb);
+    } else {
+        Kernel.handleInterupts(DISK_OPERATION_IRQ, ["unloadProcess", pcb]);
+    }
     delete ProcessManager.processControlBlocks[pcb.processId];
 };
 
@@ -45,10 +57,11 @@ ProcessManager.execute = function(pcb) {
 //
 // An interior class to represent the process control block
 //
-function ProcessControlBlock() {
+function ProcessControlBlock(inMemory) {
     // Increment the process id every time a pcb is constructed
-    this.processId = ProcessControlBlock.lastProcessId++;  
-    
+    this.processId = ProcessControlBlock.lastProcessId++;
+    this.inMemory = inMemory;
+
     this.programCounter = 0;
     this.instructionRegister = 0;
     this.accumulator = 0;
@@ -56,12 +69,12 @@ function ProcessControlBlock() {
     this.yRegister = 0;
     this.zFlag = 0;
     this.state = ProcessControlBlock.State.NEW;
-    
+
     this.output = "";
-       
+
     this.base = null;
     this.limit = null;
-    
+
     // Updates the pcb - this will be used for context switching
     this.update = function(programCounter, instructionRegister, accumulator, xRegister, yRegister, zFlag) {
         this.programCounter = programCounter;
@@ -70,6 +83,16 @@ function ProcessControlBlock() {
         this.xRegister = xRegister;
         this.yRegister = yRegister;
         this.zFlag = zFlag;
+    };
+
+    this.getProgram = function() {
+        if (this.base !== null && this.limit !== null) {
+            var program = "";
+            for (var i = 0; i < this.limit; i++) {
+                program += MemoryManager.memory.words[this.base + i].data;
+            }
+            return program;
+        }
     };
 }
 
