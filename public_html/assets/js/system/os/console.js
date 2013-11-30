f25db4e5090155d51b08f010a13586e8056eba7f
Justin Svegliato <justin.svegliato1@marist.edu>
2013-11-29 23:19:15 -0500

    Implemented line wrapping for long lines (sort of)

--- public_html/assets/js/system/os/console.js
+++ public_html/assets/js/system/os/console.js
@@ -81,20 +81,23 @@ Console.prototype.handleInput = function() {
 // Displays the specified text on the screen
 Console.prototype.putText = function(text) {
     if (text) {               
-        // Draw the text at the current X and Y coordinates
-        ConsoleDisplay.drawingContext.drawText(ConsoleDisplay.FONT_FAMILY, ConsoleDisplay.FONT_SIZE, this.xPosition, this.yPosition, text);
-        
-        if (this.xPosition >= ConsoleDisplay.CANVAS_WIDTH - ConsoleDisplay.MARGIN_OFFSET * 2) {
-            this.advanceLine();
-        } else {
-            // Move the current X position forward
-            var offset = ConsoleDisplay.drawingContext.measureText(ConsoleDisplay.FONT_FAMILY, ConsoleDisplay.FONT_SIZE, text);
-            this.xPosition += offset;
-        }            
+        var lines = text.match(/.{1,15}/g);
+        for (var i = 0; i < lines.length; i++) {
+            // Draw the text at the current X and Y coordinates
+            ConsoleDisplay.drawingContext.drawText(ConsoleDisplay.FONT_FAMILY, ConsoleDisplay.FONT_SIZE, this.xPosition, this.yPosition, lines[i]);
+
+            if (this.xPosition >= ConsoleDisplay.CANVAS_WIDTH - ConsoleDisplay.MARGIN_OFFSET * 2) {
+                this.advanceLine();
+            } else {
+                // Move the current X position forward
+                var offset = ConsoleDisplay.drawingContext.measureText(ConsoleDisplay.FONT_FAMILY, ConsoleDisplay.FONT_SIZE, lines[i]);
+                this.xPosition += offset;
+            }
+        }
     }
 };
     
-// Remvoves the specified text from the screen
+// Removes the specified text from the screen
 Console.prototype.removeText = function(text) {
     if (text !== "") {
         // Move the current X position backward
